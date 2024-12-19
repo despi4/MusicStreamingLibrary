6cb1718ebc7d14b554b22980bf81ac5fe20187de
despi4 <johapo62@gmail.com>
2024-12-19 22:08:29 +0500

    что то

--- src/Runner.java
+++ src/Runner.java
@@ -5,7 +5,7 @@ public class Runner {
         System.out.println("How should I contact you ?");
         msl.setUsername();
         String name = msl.getUsername();
-
+        System.out.println();
         System.out.println(name);
         int menu = msl.enterNumb();
         msl.printMenu();
