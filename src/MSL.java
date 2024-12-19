b939f45d605711a721982fcc3ba2b713f05a3b56
despi4 <johapo62@gmail.com>
2024-12-19 21:57:45 +0500

    First

---
+++ src/MSL.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MSL {
+    private String username;
+    int menuChoice;
+
+    public MSL(String username, int menuChoice) {
+        this.username = username;
+        this.menuChoice = menuChoice;
+    }
+
+    public int enterNumb(){
+        Scanner sc = new Scanner(System.in);
+        this.menuChoice = sc.nextInt();
+        return this.menuChoice;
+    }
+
+    public void setUsername() {
+        Scanner sc = new Scanner(System.in);
+        this.username = sc.nextLine();
+    }
+    public String getUsername() {
+        return this.username;
+    }
+
+
+
+    public static void printMenu() {
+        System.out.println("Please select one of the following options:");
+        System.out.println("1. All music\n" + "2. Add Music\n" + "3. Delete Music\n"
+                + "4. artist name\n" + "5. Create playlist\n" + "0. Exit\n");
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,37 @@
+public class Runner {
+    public static void main(String[] args) {
+        MSL msl = new MSL("User", 1);
+        System.out.println("Welcome to our music platform\n" + "_____________________________");
+        System.out.println("How should I contact you ?");
+        msl.setUsername();
+        String name = msl.getUsername();
+
+        System.out.println(name);
+        int menu = msl.enterNumb();
+        msl.printMenu();
+
+        while(true){
+            if(menu == 1){
+
+            }
+            else if(menu == 2){
+
+            }
+            else if(menu == 3){
+
+            }
+            else if(menu == 4){
+
+            }
+            else if(menu == 5){
+
+            }
+            else if(menu == 0){
+                System.out.println("Goodbye!");
+                System.exit(0);
+            }
+        }
+    }
+}
+
+
